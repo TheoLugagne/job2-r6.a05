453042c0f3ee357c83d421c122fe3aaa8cef9a79
Théo Lugagne <lugagne.theo@gmail.com>
2025-02-13 14:15:05 +0100

    init

--- test/DollarsTest.java
+++ test/DollarsTest.java
@@ -1,9 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-class DollarsTest {
+class 	DollarsTest {
 
 	@Test
 	void testDollarsDouble() {
