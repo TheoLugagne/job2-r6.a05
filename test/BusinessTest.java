1078456d459df72b8c12a0559fd992dc1291cff2
jorian.gouagout@etu.iut-tlse3.fr <jorian.gouagout@etu.iut-tlse3.fr>
2025-02-19 14:43:51 +0100

    add test

---
+++ test/BusinessTest.java
@@ -0,0 +1,62 @@
+import java.util.Date;
+
+import obj.Reading;
+import obj.Zone;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import site.BusinessSite;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessTest {
+
+	BusinessSite _subject;
+	
+	@SuppressWarnings("deprecation")
+	@BeforeEach
+	public void setUp() {
+		new Zone("A", 0.06, 0.07, new Date ("15 May 2023"), new Date ("10 Sep 2023")).register();
+		new Zone ("B", 0.07, 0.06, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+		new Zone ("C", 0.065, 0.065, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+		_subject = new BusinessSite();
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testZero() {
+		_subject.addReading(new Reading(10, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (10, new Date ("1 Feb 2023")));
+		assertEquals(0d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testChargeLessThan50Dollars() {
+		_subject.addReading(new Reading (10, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (710, new Date ("1 Feb 2023")));
+		assertEquals(50.57d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testChargeBetween50And75Dollars() {
+		_subject.addReading(new Reading (10, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (810, new Date ("1 Feb 2023")));
+		assertEquals(57.75d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testChargeGreaterThan75Dollars() {
+		_subject.addReading(new Reading (10, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (1210, new Date ("1 Feb 2023")));
+		assertEquals(86.3d, _subject.charge().amount());
+	}
+	
+	@Test
+	public void testNoReadings() {
+		try {
+			_subject.charge();
+			assert(false);
+		} catch (NullPointerException e) {}
+	}
+}
---
+++ test/DisabillityTest.java
@@ -0,0 +1,98 @@
+import java.util.Date;
+
+import obj.Reading;
+import obj.Zone;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import site.DisabilitySite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DisabillityTest {
+
+	DisabilitySite _subject;
+	
+	@SuppressWarnings("deprecation")
+	@BeforeEach
+	public void setUp() {
+		new Zone("A", 0.06, 0.07, new Date ("15 May 2023"), new Date ("10 Sep 2023")).register();
+		new Zone ("B", 0.07, 0.06, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+		new Zone ("C", 0.065, 0.065, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+		_subject = new DisabilitySite(Zone.get("A"));
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testZero() {
+		_subject.addReading(new Reading(10, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (10, new Date ("1 Feb 2023")));
+		assertEquals(0d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testSummerFractionIsEqualsTo1() {
+		_subject.addReading(new Reading (10, new Date ("16 May 2023")));
+		_subject.addReading(new Reading (110, new Date ("9 Sep 2023")));
+		assertEquals(8.14d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@DisplayName("Présence d'un potentiel bug, ce cas est non traité par le code")
+	@Test
+	public void testSummerFractionIsEqualsTo1withPartOfWinter() {
+		_subject.addReading(new Reading (10, new Date ("1 May 2023")));
+		_subject.addReading(new Reading (110, new Date ("30 Sep 2023")));
+		assertEquals(8.23d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testSummerFractionIsEqualsTo0BeforeTheSummer() {
+		_subject.addReading(new Reading (10, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (110, new Date ("1 Feb 2023")));
+		assertEquals(9.19d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testSummerFractionIsEqualsTo0AfterTheSummer() {
+		_subject.addReading(new Reading (10, new Date ("1 Oct 2023")));
+		_subject.addReading(new Reading (110, new Date ("1 Nov 2023")));
+		assertEquals(9.19d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testSummerFractionEndIsInTheSummer() {
+		_subject.addReading(new Reading (10, new Date ("1 May 2023")));
+		_subject.addReading(new Reading (110, new Date ("5 Sep 2023")));
+		assertEquals(8.25d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testSummerFractionStartIsInTheSummer() {
+		_subject.addReading(new Reading (10, new Date ("16 May 2023")));
+		_subject.addReading(new Reading (110, new Date ("7 Oct 2023")));
+		assertEquals(8.34d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	@DisplayName("Doit être inférieur que our les résidents, application de CAP à 200")
+	public void testSummerFractionStartIsInTheSummerWithUsageMoreThan200() {
+		_subject.addReading(new Reading (10, new Date ("16 May 2023")));
+		_subject.addReading(new Reading (310, new Date ("7 Oct 2023")));
+		assertEquals(24.86d, _subject.charge().amount());
+	}
+
+	@Test
+	public void testNoReadings() {
+		try {
+			_subject.charge();
+			assert(false);
+		} catch (ArrayIndexOutOfBoundsException e) {}
+	}
+}
